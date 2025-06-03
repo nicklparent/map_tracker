652b3169f7ed5020d02a71d38020f2cc4e205c88
nick.lparent <nick.lparent@outlook.com>
2025-06-02 23:02:35 -0300

    login backend complete

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,12 +1,14 @@
 import { prisma } from "@/lib/db"
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { signJwt } from "@/lib/jwt";
+import { JwtPayload } from "@/types";
 
 export async function POST(req: NextRequest){
     const {email, password} = await req.json();
 
     try {
-        const user = prisma.User.findUnique({
+        const user = await prisma.User.findUnique({
             where: { email }
         });
 
@@ -17,13 +19,23 @@ export async function POST(req: NextRequest){
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
         if (!isPasswordCorrect) {
-            return NextResponse.json({ error: "Invalid Password"}, { status: 401 });
+            return NextResponse.json({ error: "Incorrect Password"}, { status: 401 });
         }
         
-
+        
         const { password: _, ...userWithoutPassword} = user;
-        // Return succesful login without password on user
-        return NextResponse.json({ user: userWithoutPassword}, { status: 200 });
+
+        //create a 30 day session after succesful login
+        const token = signJwt({
+            id: userWithoutPassword.id, 
+            email: userWithoutPassword.email
+        }, "30d");
+
+        // Return succesful login without password on user and a jwt token
+        return NextResponse.json({ 
+            user: userWithoutPassword,
+            token: token,
+        }, { status: 200 });
     } catch (e) {
         return NextResponse.json({error: "User Credentials invalid."}, { status: 401 });
     }
